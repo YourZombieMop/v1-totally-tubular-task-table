e032e24e55f76e405b62f2ec3fec592b35e682d5
Matthew Boyea <yourzombiemop@gmail.com>
2022-05-26 11:18:52 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+/**
+ * This App should:
+ *  - Input a task
+ *  - Add the task to a list
+ *  - Display the list of tasks
+ *  - Allow for completing a task
+ *  - Allow for deleting a task
+ *  - Add Completed tasks to a done list
+ *
+ * Extras:
+ *  - Style the App
+ *  - Edit an existing task
+ */
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello ToDo List</h1>
+    </div>
+  );
+}
---
+++ src/components/Todo.tsx
@@ -0,0 +1,12 @@
+/**
+ * This App should:
+ *  - Input a task
+ *  - Add the task to a list
+ *  - Display the list of tasks
+ *  - Allow for completing a task
+ *  - Allow for deleting a task
+ *  - Add that item to a compeleted list
+ */
+const Todo = () => {};
+
+export default Todo;
